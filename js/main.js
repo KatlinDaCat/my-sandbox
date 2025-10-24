881dcadcade041d082a5ac31f6a9092db4259f93
Katlin <Kaden1084@gmail.com>
2025-10-24 11:24:32 -0500

    fix stuff

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { getCanvas, loadImage, global, Sprite } from "./js/util.js";
+import { getCanvas, loadImage, global, Sprite } from "./util.js";
 
 const cvs = getCanvas('canvas');
 const ctx = cvs.getContext('2d');
