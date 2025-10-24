1e9f36bb4c6685d3194ee3983e42f6df3a088a0d
Katlin <Kaden1084@gmail.com>
2025-10-23 20:49:04 -0500

    add stuff

--- js/main.js
+++ js/main.js
@@ -5,3 +5,38 @@ const ctx = cvs.getContext('2d');
 
 /* Setup */
 
+global.keys = {};
+
+document.addEventListener('keydown', (e) => {
+    global.keys[e.key] = true;
+});
+
+document.addEventListener('keyup', (e) => {
+    global.keys[e.key] = false;
+});
+
+/* Other stuff I don't know how to describe */
+
+function update(deltaTime) {
+    for (const sprite of global.sprites) {
+        sprite._update_(deltaTime);
+    }
+}
+
+function draw() {
+
+}
+
+let lastFrameTime = 0;
+
+function gameLoop(currentTime) {
+    const deltaTime = (currentTime - lastFrameTime) / 1000;
+    lastFrameTime = currentTime;
+
+    update(deltaTime);
+    draw();
+
+    window.requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
